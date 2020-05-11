ef8156a6578453ed4fc22007e45272a2df444ec8
nit <netniti000@yahoo.co.jp>
2020-05-11 23:03:07 +0000

    add new recipe creation with test

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -33,7 +33,16 @@ end
   
   test "create new valid recipe" do
     get new_recipe_path
-  end
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add vegetables, cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+    post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+end
   
   test "reject invalid recipe submissions" do
     get new_recipe_path
