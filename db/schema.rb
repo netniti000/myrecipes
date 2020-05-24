ede8b3ff1de78357483ae66411f46bd895ee3fdf
nit <netniti000@yahoo.co.jp>
2020-05-24 13:32:32 +0000

    admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_144033) do
+ActiveRecord::Schema.define(version: 2020_05_24_131711) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_144033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
